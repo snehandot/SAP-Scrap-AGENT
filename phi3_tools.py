a1db56207ad6c8af316f9afdb37d3f02c04a534b
SanjayPalanisami <138524201+SanjayPalanisami@users.noreply.github.com>
2024-06-21 14:37:57 +0530

    Add files via upload

--- phi3_tools.py
+++ phi3_tools.py
@@ -112,73 +112,73 @@
 # #     main()
 
 # ####### kuhan nix code
-# # import argparse
-# # from langchain_community.vectorstores import Chroma
-# # from langchain.prompts import ChatPromptTemplate
-# # from langchain_community.llms import LlamaCpp
-# # from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
-# # from get_embedding_function import get_embedding_function
+# import argparse
+# from langchain_community.vectorstores import Chroma
+# from langchain.prompts import ChatPromptTemplate
+# from langchain_community.llms import LlamaCpp
+# from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+# from get_embedding_function import get_embedding_function
 
-# # CHROMA_PATH = "chroma"
+# CHROMA_PATH = "chroma"
 
-# # PROMPT_TEMPLATE = """
-# # Answer the question based only on the following context:
+# PROMPT_TEMPLATE = """
+# Answer the question based only on the following context:
 
-# # {context}
+# {context}
 
-# # ---
+# ---
 
-# # Answer the question based on the above context: {question}
-# # """
+# Answer the question based on the above context: {question}
+# """
 
-# # def main():
-# #     # Create CLI.
-# #     parser = argparse.ArgumentParser()
-# #     parser.add_argument("query_text", type=str, help="The query text.")
-# #     args = parser.parse_args()
+# def main():
+#     # Create CLI.
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("query_text", type=str, help="The query text.")
+#     args = parser.parse_args()
     
-# #     # Pass the command line argument to the function.
-# #     query_rag(args.query_text)
+#     # Pass the command line argument to the function.
+#     query_rag(args.query_text)
 
-# # def query_rag(query_text: str):
-# #     # Prepare the DB.
-# #     embedding_function = get_embedding_function()
-# #     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+# def query_rag(query_text: str):
+#     # Prepare the DB.
+#     embedding_function = get_embedding_function()
+#     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-# #     # Search the DB.
-# #     results = db.similarity_search_with_score(query_text, k=5)
+#     # Search the DB.
+#     results = db.similarity_search_with_score(query_text, k=5)
 
-# #     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-# #     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-# #     prompt = prompt_template.format(context=context_text, question=query_text)
-# #     print(prompt)
+#     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+#     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+#     prompt = prompt_template.format(context=context_text, question=query_text)
+#     print(prompt)
 
-# #     # Set up the LlamaCpp model
-# #     n_gpu_layers = -1
-# #     n_batch = 512
-# #     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+#     # Set up the LlamaCpp model
+#     n_gpu_layers = -1
+#     n_batch = 512
+#     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     
-# #     # Make sure the model path is correct for your system!
-# #     model_path = "/Users/sanjaypalanisami/5AP/Phi-3-mini-4k-instruct-function-calling_Q6_K.gguf"
-# #     llm = LlamaCpp(
-# #         model_path=model_path,
-# #         n_gpu_layers=n_gpu_layers,
-# #         n_batch=n_batch,
-# #         callback_manager=callback_manager,
-# #         verbose=True,  # Verbose is required to pass to the callback manager
-# #     )
+#     # Make sure the model path is correct for your system!
+#     model_path = "/Users/sanjaypalanisami/5AP/Phi-3-mini-4k-instruct-function-calling_Q6_K.gguf"
+#     llm = LlamaCpp(
+#         model_path=model_path,
+#         n_gpu_layers=n_gpu_layers,
+#         n_batch=n_batch,
+#         callback_manager=callback_manager,
+#         verbose=True,  # Verbose is required to pass to the callback manager
+#     )
     
-# #     # Generate a response using the LlamaCpp model
-# #     llm_chain = prompt_template | llm
-# #     response_text = llm_chain.invoke({"question": query_text})
+#     # Generate a response using the LlamaCpp model
+#     llm_chain = prompt_template | llm
+#     response_text = llm_chain.invoke({"question": query_text})
 
-# #     sources = [doc.metadata.get("id", None) for doc, _score in results]
-# #     formatted_response = f"Response: {response_text}\nSources: {sources}"
-# #     print(formatted_response)
-# #     return response_text
+#     sources = [doc.metadata.get("id", None) for doc, _score in results]
+#     formatted_response = f"Response: {response_text}\nSources: {sources}"
+#     print(formatted_response)
+#     return response_text
 
-# # if __name__ == "__main__":
-# #     main()
+# if __name__ == "__main__":
+#     main()
 
 #################.   working ###################### 
 
@@ -250,18 +250,141 @@
 # if __name__ == "__main__":
 #     main()
 
+############################## WORKNG CODE ################################
+
+# import argparse
+# from langchain_community.vectorstores import Chroma
+# from langchain.prompts import ChatPromptTemplate
+# from langchain_community.llms import LlamaCpp
+# from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+# from get_embedding_function import get_embedding_function
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+# import torch
+
+# CHROMA_PATH = "chroma"
+
+# PROMPT_TEMPLATE = """
+# Answer the question based only on the following context:
+
+# {context}
+
+# ---
+
+# Answer the question based on the above context: {question}
+# """
+
+# def main():
+#     # Create CLI.
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("query_text", type=str, help="The query text.")
+#     args = parser.parse_args()
+    
+#     # Pass the command line argument to the function.
+#     query_rag(args.query_text)
+
+# def query_rag(query_text: str):
+#     # Prepare the DB.
+#     embedding_function = get_embedding_function()
+#     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+#     # Search the DB.
+#     results = db.similarity_search_with_score(query_text, k=5)
+
+#     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+#     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+#     prompt = prompt_template.format(context=context_text, question=query_text)
+#     print(prompt)
+
+#     # Set up the LlamaCpp model
+#     n_gpu_layers = -1
+#     n_batch = 2000
+#     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+    
+#     # Make sure the model path is correct for your system!
+#     model_path = "/Users/sanjaypalanisami/5AP/Phi-3-mini-4k-instruct-function-calling_Q6_K.gguf"
+#     llm = LlamaCpp(
+#         model_path=model_path,
+#         n_gpu_layers=n_gpu_layers,
+#         n_batch=n_batch,
+#         n_ctx = 2048,
+#         callback_manager=callback_manager,
+#         verbose=True,  # Verbose is required to pass to the callback manager
+#     )
+    
+#     # Generate a response using the LlamaCpp model
+#     response_text = llm.invoke(prompt)
+
+#     sources = [doc.metadata.get("id", None) for doc, _score in results]
+#     formatted_response = f"Response: {response_text}\nSources: {sources}"
+#     print(formatted_response)
+
+#     # Integrate the search tool
+#     #integrate_search_tool(query_text)
+
+#     return response_text
+
+# # def integrate_search_tool(query_text: str):
+# #     model_id = "mzbac/Phi-3-mini-4k-instruct-function-calling"
+# #     tokenizer = AutoTokenizer.from_pretrained(model_id)
+# #     model = AutoModelForCausalLM.from_pretrained(
+# #         model_id,
+# #         torch_dtype=torch.bfloat16,
+# #         device_map="auto",
+# #     )
+
+# #     tool = {
+# #         "name": "search_web",
+# #         "description": "Perform a web search for a given search terms.",
+# #         "parameter": {
+# #             "type": "object",
+# #             "properties": {
+# #                 "search_terms": {
+# #                     "type": "array",
+# #                     "items": {"type": "string"},
+# #                     "description": "The search queries for which the search is performed.",
+# #                     "required": True,
+# #                 }
+# #             },
+# #         },
+# #     }
+
+# #     messages = [
+# #         {
+# #             "role": "user",
+# #             "content": f"You are a helpful assistant with access to the following functions. Use them if required - {str(tool)}",
+# #         },
+# #         {"role": "user", "content": f"Perform a web search for the following query: {query_text}"},
+# #     ]
+
+# #     input_ids = tokenizer.apply_chat_template(
+# #         messages, add_generation_prompt=True, return_tensors="pt"
+# #     ).to(model.device)
+
+# #     terminators = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("")]
+
+# #     outputs = model.generate(
+# #         input_ids,
+# #         max_new_tokens=256,
+# #         eos_token_id=terminators,
+# #         do_sample=True,
+# #         temperature=0.1,
+# #     )
+# #     response = outputs[0]
+# #     print(tokenizer.decode(response))
+
+# if __name__ == "__main__":
+#      main()
+
+################################## WORKING CODE ENDS HERE ################################
+     
 
 
 import argparse
-from langchain_community.vectorstores import Chroma
+from langchain.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms import LlamaCpp
-from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
 from get_embedding_function import get_embedding_function
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-
-CHROMA_PATH = "chroma"
+import os
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
@@ -274,136 +397,54 @@ Answer the question based on the above context: {question}
 """
 
 def main():
-    # # Create CLI.
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("query_text", type=str, help="The query text.")
-    # args = parser.parse_args()
-    query_text="write me an 1000 word essay on love"
-    # Pass the command line argument to the function.
-    query_rag(query_text)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    parser.add_argument("country", type=str, help="The country name.")
+    args = parser.parse_args()
+    
+    query_rag(args.query_text, args.country)
 
-def query_rag(query_text: str):
-    # Prepare the DB.
+def query_rag(query_text: str, country: str):
+    CHROMA_PATH = os.path.join("chroma", country)
+    if not os.path.exists(CHROMA_PATH):
+        print(f"No data found for country: {country}")
+        return
+    
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-    # Search the DB.
-    results = db.similarity_search_with_score(query_text, k=5)
+    results = db.similarity_search_with_score(query_text, k=6)
+    # print(results)
+
+    for doc, score in results:
+        print(f"Document: {doc.page_content}\nScore: {score}\n")
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+
+    print(context_text)
+    print('\nhi333333333333333333333\n')
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
-
-    # Set up the LlamaCpp model
-    n_gpu_layers = 1000
-    n_batch = 2000
-    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     
-    # Make sure the model path is correct for your system!
-    model_path = "//Users/alferix/Documents/models/phi-3-mini-128k-instruct_function.q8_0.gguf"
+    print("Generated Prompt:", prompt)
+
+    n_gpu_layers = -1   
+    n_batch = 1000
+    model_path = "/Users/sanjaypalanisami/5AP/Phi-3-mini-4k-instruct-function-calling_Q6_K.gguf"
     llm = LlamaCpp(
         model_path=model_path,
         n_gpu_layers=n_gpu_layers,
         n_batch=n_batch,
-        n_threads=10,
-        n_ctx = 2048,
-        callback_manager=callback_manager,
-        verbose=True,  # Verbose is required to pass to the callback manager
-        low_memory=False,
-        use_mmap=False
+        n_ctx=10000,
+        verbose=True,
     )
     
-    # Generate a response using the LlamaCpp model
     response_text = llm.invoke(prompt)
+    print("Model Response:", response_text)
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
-    #print(formatted_response)
-
-    tool = {
-        "name": "search_web",
-        "description": "Perform a web search for a given search terms.",
-        "parameter": {
-            "type": "object",
-            "properties": {
-                "search_terms": {
-                    "type": "array",
-                    "items": {"type": "string"},
-                    "description": "The search queries for which the search is performed.",
-                    "required": True,
-                }
-            },
-        },
-    }
-
-    messages = [
-        {
-            "role": "user",
-            "content": f"You are a helpful assistant with access to the following functions. Use them if required - {str(tool)}",
-        },
-        {"role": "user", "content": f"Perform a web search for the following query: {query_text}"},
-    ]
-
-    # Integrate the search tool
-    # response_text = llm.invoke(messages)
-
-    # sources = [doc.metadata.get("id", None) for doc, _score in results]
-    # formatted_response = f"Response: {response_text}\nSources: {sources}"
-    # print(formatted_response)
-    #integrate_search_tool(llm,query_text)
-
-    return response_text
-
-def integrate_search_tool(model,query_text: str):
-    # model_id = "mzbac/Phi-3-mini-4k-instruct-function-calling"
-    # tokenizer = AutoTokenizer.from_pretrained(model_id)
-    # model = AutoModelForCausalLM.from_pretrained(
-    #     model_id,
-    #     torch_dtype=torch.bfloat16,
-    #     device_map="auto",
-    # )
-
-    tool = {
-        "name": "search_web",
-        "description": "Perform a web search for a given search terms.",
-        "parameter": {
-            "type": "object",
-            "properties": {
-                "search_terms": {
-                    "type": "array",
-                    "items": {"type": "string"},
-                    "description": "The search queries for which the search is performed.",
-                    "required": True,
-                }
-            },
-        },
-    }
-
-    messages = [
-        {
-            "role": "user",
-            "content": f"You are a helpful assistant with access to the following functions. Use them if required - {str(tool)}",
-        },
-        {"role": "user", "content": f"Perform a web search for the following query: "},
-    ]
-
-    input_ids = tokenizer.apply_chat_template(
-        messages, add_generation_prompt=True, return_tensors="pt"
-    ).to(model.device)
-
-    terminators = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("")]
-
-    outputs = model.generate(
-        input_ids,
-        max_new_tokens=256,
-        eos_token_id=terminators,
-        do_sample=True,
-        temperature=0.1,
-    )
-    response = outputs[0]
-    print(tokenizer.decode(response))
+    print(formatted_response)
 
 if __name__ == "__main__":
     main()
-
